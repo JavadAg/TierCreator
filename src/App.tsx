1a0fffa7eca53c4ec7e3c06f24df43fe5ff24f6e
JavadAg <j.aghebati93@gmail.com>
2022-07-26 23:49:00 +0430

    added navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
+import React from "react"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 import Navbar from "./components/Navbar/Navbar"
+import Home from "./pages/Home"
 
 function App() {
   return (
-    <>
-      <Navbar />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   )
 }
 
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,7 +1,42 @@
 import React from "react"
+import logo from "../../assets/logo.svg"
+import { FiSearch } from "react-icons/fi"
+import userEvent from "@testing-library/user-event"
+import { Link } from "react-router-dom"
 
 const Navbar = () => {
-  return <div>Navbar</div>
+  const user = {
+    name: "Javad"
+  }
+  return (
+    <div className="flex justify-between items-center p-2 bg-zinc-800 text-slate-200">
+      <div className="flex justify-center items-center">
+        <Link to="/">
+          <img src={logo} alt="tiercreator-logo" className="h-10" />
+        </Link>
+      </div>
+      <div className="flex justify-center items-center space-x-4">
+        <i>
+          <FiSearch />
+        </i>
+        <Link
+          to="/create"
+          className="bg-indigo-600 hover:bg-indigo-500 duration-200 px-2 py-1 rounded-md cursor-pointer"
+        >
+          Make a Tier
+        </Link>
+        <Link to="/categories" className="cursor-pointer">
+          Categories
+        </Link>
+        <Link to="/tier-lists/new" className="cursor-pointer">
+          New Tier Lists
+        </Link>
+        <Link to="/dashboard" className="capitalize cursor-pointer">
+          {user.name}
+        </Link>
+      </div>
+    </div>
+  )
 }
 
 export default Navbar
